7021c2350f42172f8f1472d7b1b755a98c7bbb2b
sibkoh <78790005+sibkoh@users.noreply.github.com>
2025-02-02 14:10:19 +0100

    Desde programacion a Entornos de Desarrollo Entregado pre examenes Febrero 2025

---
+++ Asignaturas/Entornos de Desarrollo/UD3/Caso Practico 1/Multiplicacion.java
@@ -0,0 +1,15 @@
+public class Multiplicacion {
+	private int num1;
+	private int num2;
+
+	// Constructor
+	public Multiplicacion(int num1, int num2) {
+		this.num1 = num1;
+		this.num2 = num2;
+	}
+
+	// Método para realizar la multiplicación
+	public int multiplicar() {
+		return num1 * num2;
+	}
+}
