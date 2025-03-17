c880e6e85bd7ae963b906207e80804251361b785
sibkoh <78790005+sibkoh@users.noreply.github.com>
2025-03-17 16:00:26 +0100

    UD6 Programacion Caso practico terminado

---
+++ Asignaturas/Programación/Proyectos Eclipse/GestionEstudiantes/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // Crear una instancia de la clase Menu
+        Menu menu = new Menu();
+        
+        // Llamar al método mostrarMenuPrincipal usando la instancia del objeto
+        menu.mostrarMenuPrincipal();
+    }
+}
