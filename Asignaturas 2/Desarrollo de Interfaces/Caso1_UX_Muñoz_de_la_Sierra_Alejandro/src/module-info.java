b4ee0b41e05c5df67cf7003108dd49f238fcd8bd
sibkoh <78790005+sibkoh@users.noreply.github.com>
2025-10-17 12:51:03 +0200

    faltan interfaces arreglarla

--- Asignaturas 2/Desarrollo de Interfaces/Caso1_UX_Muñoz_de_la_Sierra_Alejandro/src/module-info.java
+++
@@ -1,7 +0,0 @@
-module TareaUD1 {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires transitive javafx.graphics; // <--- asegura que cualquier cliente vea Stage
-    exports application; // IMPORTANTE: ahora exportamos el paquete 'application'
-}
-
