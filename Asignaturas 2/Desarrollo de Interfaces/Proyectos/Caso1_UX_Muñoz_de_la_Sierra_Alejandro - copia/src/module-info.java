dbbd8eb76f2c3e4f880e5e0735248a6f517c37bc
sibkoh <78790005+sibkoh@users.noreply.github.com>
2025-10-20 00:09:18 +0200

    pre entrega de Interfaces

---
+++ Asignaturas 2/Desarrollo de Interfaces/Proyectos/Caso1_UX_Muñoz_de_la_Sierra_Alejandro - copia/src/module-info.java
@@ -0,0 +1,8 @@
+module Caso1_UX_Muñoz_de_la_Sierra_Alejandro {
+	requires javafx.controls;
+	requires javafx.graphics;
+	requires javafx.fxml;
+
+	opens application to javafx.graphics, javafx.fxml;
+
+}
