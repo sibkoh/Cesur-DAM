47fc965fab0a7bf1ea707daa823ff637ec23a98b
sibkoh <78790005+sibkoh@users.noreply.github.com>
2025-11-17 10:16:41 +0100

    gitignore arreglado

--- Asignaturas/Programación/Proyectos Eclipse/GestionEstudiantes/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        // Crear una instancia de la clase Menu
-        Menu menu = new Menu();
-        
-        // Llamar al método mostrarMenuPrincipal usando la instancia del objeto
-        menu.mostrarMenuPrincipal();
-    }
-}
--- Asignaturas/Programación/Proyectos Eclipse/mifx/src/module-info.java
+++
@@ -1,5 +0,0 @@
-module mifx {
-	requires javafx.controls;
-	
-	opens application to javafx.graphics, javafx.fxml;
-}
